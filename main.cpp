c90a745fd5af6b163297a001b222ebaf3d86254d
Nikita Krenov <nikitabaram@gmail.com>
2024-06-08 16:17:16 +0300

    positive or negative num but we use function

--- main.cpp
+++ main.cpp
@@ -3,7 +3,37 @@
 
 using namespace std;
 
+bool fS(int* num, int* sign) {
+	if (*num > 0) {
+		*sign = 1;
+		return true;
+	}
+	else if (*num < 0) {
+		*sign = -1;
+		return true;
+	}
+	else {
+		*sign = 0;
+		return false;
+	}
+}
+
 int main() {
+	int number, sign;
+	cout << "Enter a number: ";
+	cin >> number;
+
+	fS(&number, &sign);
+
+	if (sign == 0) {
+		cout << "The number is zero." << endl;
+	}
+	else if (sign > 0) {
+		cout << "The number is positive." << endl;
+	}
+	else {
+		cout << "The number is negative." << endl;
+	}
 
 	system("pause");
 	return 0;
