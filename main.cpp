67a1ffdeedaabc8aeb7db8e28aef0bc24a6ad070
Nikita Krenov <nikitabaram@gmail.com>
2024-06-08 17:28:01 +0300

    sum all numbers in arr

--- main.cpp
+++ main.cpp
@@ -1,10 +1,30 @@
-#include <iostream>;
-#include <string>;
+#include <iostream>
 
 using namespace std;
 
-int main() {
+int sumArr(int* arr, int size) {
+    int sum = 0;
+    for (int* i = arr; i < arr + size; i++) {
+        sum += *i;
+    }
+    return sum;
+}
 
-	system("pause");
-	return 0;
+int main() {
+    int size;
+    cout << "Enter the size of the array: ";
+    cin >> size;
+
+    int* arr = new int[size];
+
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < size; ++i) {
+        cin >> arr[i];
+    }
+
+    int sum = sumArr(arr, size);
+    cout << "The sum of the elements of the array is: " << sum << endl;
+
+    delete[] arr;
+    return 0;
 }
