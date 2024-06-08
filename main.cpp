fc3fb18313fd7081be1c1712a7efb098236388ac
Nikita Krenov <nikitabaram@gmail.com>
2024-06-08 16:22:08 +0300

    change two numbers with him

--- main.cpp
+++ main.cpp
@@ -3,7 +3,26 @@
 
 using namespace std;
 
+bool change(int* a, int* b) {
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+	return true;
+}
+
 int main() {
+	int num1, num2;
+	cout << "Enter the first number: ";
+	cin >> num1;
+
+	cout << "Enter the second number: ";
+	cin >> num2;
+
+	cout << "Before changing: " << num1 << " " << num2 << endl;
+
+	change(&num1, &num2);
+
+	cout << "After changing: " << num1 << " " << num2 << endl;
 
 	system("pause");
 	return 0;
