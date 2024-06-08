3847d8db75cd9aef35243039491a7e33de92d334
Nikita Krenov <nikitabaram@gmail.com>
2024-06-08 16:08:00 +0300

    find the max number use function

--- main.cpp
+++ main.cpp
@@ -3,7 +3,28 @@
 
 using namespace std;
 
+bool find_the_max_number(int* a, int* b, int* max) {
+	if (*a > *b) {
+		*max = *a;
+		return true;
+	}
+	else {
+		*max = *b;
+		return false;
+	
+	}
+}
+
 int main() {
+	int num1, num2, max;
+	cout << "Enter the first number: ";
+	cin >> num1;
+	cout << "Enter the second number: ";
+	cin >> num2;
+
+	find_the_max_number(&num1, &num2, &max);
+	t << "The maximum number is: " << max << endl;
+
 
 	system("pause");
 	return 0;
