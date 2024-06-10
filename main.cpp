90998cabfcaf130af7ef39086b46620d77764b2c
Nikita Krenov <nikitabaram@gmail.com>
2024-06-10 23:03:14 +0300

    maybe good

--- main.cpp
+++ main.cpp
@@ -4,7 +4,24 @@
 using namespace std;
 
 int main() {
+	const int SIZE = 5;
+	int sourceArray[SIZE] = { 1, 2, 3, 4, 5 };
+	int destinationArray[SIZE];
+	int* sourcePtr = sourceArray;
+	int* destinationPtr = destinationArray;
+
+	for (int i = 0; i < SIZE; i++) {
+		*destinationPtr = *sourcePtr;
+		sourcePtr++;
+		destinationPtr++;
+	}
+	
+	cout << "Copy arr: ";
+	for (int i = 0; i < SIZE; i++) {
+		cout << destinationArray[i] << " ";
+	}
 
 	system("pause");
 	return 0;
+
 }
