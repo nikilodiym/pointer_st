ea363c45846f7b7d79a261631ba834e7e6212a59
Nikita Krenov <nikitabaram@gmail.com>
2024-06-13 17:24:18 +0300

    reversed

--- main.cpp
+++ main.cpp
@@ -3,7 +3,41 @@
 
 using namespace std;
 
+void reverseArray(int* arr, int size) {
+    int* start = arr;
+    int* end = arr + size - 1;
+
+    while (start < end) {
+        int temp = *start;
+        *start = *end;
+        *end = temp;
+
+        start++;
+        end--;
+    }
+}
+
 int main() {
+    int size;
+    cout << "Enter the size of the array: ";
+    cin >> size;
+
+    int* arr = new int[size];
+
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < size; ++i) {
+        cin >> arr[i];
+    }
+
+    reverseArray(arr, size);
+
+    cout << "Reversed array: ";
+    for (int i = 0; i < size; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    delete[] arr;
 
 	system("pause");
 	return 0;
