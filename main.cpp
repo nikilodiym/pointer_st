6c19341049d32941dc703838f2845610b0d7d95b
Nikita Krenov <nikitabaram@gmail.com>
2024-06-13 17:40:26 +0300

    last 3 task

--- main.cpp
+++ main.cpp
@@ -3,7 +3,40 @@
 
 using namespace std;
 
+void reverseCopyArray(int* source, int* destination, int size) {
+    int* src = source;
+    int* dest = destination + size - 1;
+
+    while (src < source + size) {
+        *dest = *src;
+        src++;
+        dest--;
+    }
+}
+
 int main() {
+    int size;
+    cout << "Enter the size of the array: ";
+    cin >> size;
+
+    int* sourceArray = new int[size];
+    int* destinationArray = new int[size];
+
+    cout << "Enter the elements of the source array: ";
+    for (int i = 0; i < size; ++i) {
+        cin >> sourceArray[i];
+    }
+
+    reverseCopyArray(sourceArray, destinationArray, size);
+
+    cout << "Reversed array: ";
+    for (int i = 0; i < size; ++i) {
+        cout << destinationArray[i] << " ";
+    }
+    cout << endl;
+
+    delete[] sourceArray;
+    delete[] destinationArray;
 
 	system("pause");
 	return 0;
